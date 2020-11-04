7cc889a7eff170761bd04ad9e8cf9a84cbcdcf89
juampacamarda <jpcamarda265@gmail.com>
2020-11-04 17:02:29 -0300

    portfolio link home

--- wp-content/themes/theme-cygnus/assets/inc/home/companies-content.php
+++ wp-content/themes/theme-cygnus/assets/inc/home/companies-content.php
@@ -7,7 +7,10 @@
       <h2 class="d-none d-lg-block"><?php the_field( 'titulo_companies' ); ?></h2>
       <h3 class="d-block d-lg-none"><?php the_field( 'titulo_companies' ); ?></h3>
       <p><?php the_field( 'frase_companies' ); ?></p>
-      <a href="" class="enlace">See all companies</a>
+      <?php $enlace_portfolio = get_field( 'enlace_portfolio' ); ?>
+      <?php if ( $enlace_portfolio ) : ?>
+      <a href="<?php echo esc_url( $enlace_portfolio) ; ?>" class="enlace">See all companies</a>
+      <?php endif; ?>
     </div>
   </div>
 </div>
